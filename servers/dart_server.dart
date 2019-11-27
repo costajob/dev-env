cdd887f5e69f6ca0250637a1e53254d99aaec378
Igor Demyanov <igor.demyanov@team.wrike.com>
2019-11-27 16:42:20 +0300

    update nodejs && dart server, fix excess isolate count

--- servers/dart_server.dart
+++ servers/dart_server.dart
@@ -6,16 +6,19 @@ const String _HOST = '0.0.0.0';
 const String _GREET = 'Hello World';
 
 _startServer(arg) async {
-  var server = await HttpServer.bind(_HOST, 9292, shared: true);
-  await for (HttpRequest request in server) {
-    request.response
+  print('Start server');
+  final server = await HttpServer.bind(_HOST, 9292, shared: true);
+  server.listen((HttpRequest request) {
+      request.response
       ..write(_GREET)
       ..close();
-  }
+  });
 }
 
-void main() {
-  for (int i = 0; i < Platform.numberOfProcessors; i++)
+void main(List<String> args) {
+  final threads = (args.length>0?int.parse(args[0]):Platform.numberOfProcessors);
+  print('CPU: ${Platform.numberOfProcessors}, Threads: ${threads}');
+  for (int i = 1; i < threads; i++)
     Isolate.spawn(_startServer, null);
   _startServer(null);
 }
--- servers/node_server.js
+++ servers/node_server.js
@@ -1,12 +1,14 @@
 const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
-
+const threads = process.argv.length>2?parseInt(process.argv[2]):numCPUs;
+console.log('CPU: ',numCPUs, 'threads: ',threads);
 if (cluster.isMaster) {
-  for (let i = 0; i < numCPUs; i++) {
+  for (let i = 0; i < threads; i++) {
     cluster.fork();
   }
 } else {
+  console.log('Start server');
   http.createServer((req, res) => {
     res.writeHead(200);
     res.end('Hello World');
